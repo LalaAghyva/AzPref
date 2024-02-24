09978258fb7cfc6bf12d9fcdc61ae2a5c891d28e
Lala <laleagayeva78@gmail.com>
2024-02-25 00:33:42 +0400

    Initial Commit

---
+++ AzPref/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AzPref
+//
+//  Created by Lala on 25.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
